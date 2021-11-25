8709a77ece05fbfd3d5ea220ad4d733fa4ab2be2
rahmat <rahmatalfathb@gmail.com>
2021-11-25 09:02:32 +0700

    Tugas JS Dasar Niomic

---
+++ Tugas1.js
@@ -0,0 +1,8 @@
+var one = "Rahmat";
+let two = "Alfath";
+
+const three = 26;
+
+console.log(one)
+console.log(two)
+console.log(three)
---
+++ Tugas2.js
@@ -0,0 +1,14 @@
+const one = 26;
+let two = "Saya adalah";
+var tujuan = true;
+var three = "ada";
+three = null;
+var four;
+
+var angka = [1, 2, 3, 4, 5, 6, 9]
+
+console.log(one)
+console.log(two)
+console.log(three)
+console.log(four)
+console.log(angka[4])
